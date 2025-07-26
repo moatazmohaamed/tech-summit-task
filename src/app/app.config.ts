279a08890e4d0d7cd8891a39d0d955224b5c321f
Moataz Mohamed <moataz9898k@gmail.com>
2025-07-26 21:26:37 +0300

    Modified the Hash location Added smooth animation between pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation, withInMemoryScrolling, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import {
@@ -12,7 +12,9 @@ import { provideHotToastConfig } from '@ngxpert/hot-toast';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withViewTransitions(),
+      withHashLocation(),
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' })),
     provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch()), provideHotToastConfig(),
   ],
